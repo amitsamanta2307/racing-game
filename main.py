d3e596264ecd110ab63a3e7c1d97ced78d5cbf5d
Amit Samanta <developer.amitsamanta@gmail.com>
2021-07-28 15:54:40 +0530

    Add the racing the logic for the turtles

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import random
 is_race_on = False
 colors = ["green", "orange", "indigo", "yellow", "red", "violet"]
 y_positions = [-70, -40, -10, 20, 50, 80]
+all_turtles = []
 
 screen = Screen()
 screen.setup(width=500, height=400)
@@ -15,5 +16,24 @@ for index in range(0, 6):
     new_turtle.penup()
     new_turtle.color(colors[index])
     new_turtle.goto(-230, y_positions[index])
+    all_turtles.append(new_turtle)
+
+print(f"User input {user_bet}")
+if user_bet:
+    is_race_on = True
+
+while is_race_on:
+
+    for turtle in all_turtles:
+        if turtle.xcor() > 220:
+            is_race_on = False
+            winning_color = turtle.pencolor()
+            if winning_color == user_bet.lower():
+                print(f"You've won! The {winning_color} turtle is the winner!")
+            else:
+                print(f"You've lost! The {winning_color} turtle is the winner!")
+
+        random_distance = random.randint(0, 10)
+        turtle.forward(random_distance)
 
 screen.exitonclick()
